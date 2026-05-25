536d44ab5a87917fa97429be13d3e4efc73d5045
Juan Esteban Echavarría Corrales <juanestebanechavarria511@gmail.com>
2026-05-25 01:27:42 -0500

    implementacion de bfs

--- algoritmos.py
+++ algoritmos.py
@@ -63,3 +63,39 @@ def bfs_costos(grafo: Grafo, origen: str, destino: str,alpha: float, beta: float
     heap = [(0.0, origen, [origen])]
     costo_minimo: dict[str, float] = {origen: 0.0}
     nodos_explorados = 0
+
+    while heap:
+        costo_actual, nodo_actual, camino = heapq.heappop(heap)
+        nodos_explorados += 1
+
+        if nodo_actual == destino:
+            tiempo_ms = (time.perf_counter() - inicio) * 1000
+            return ResultadoRuta(
+                algoritmo="BFS Adaptado",
+                ruta=camino,
+                costo_total=costo_actual,
+                nodos_explorados=nodos_explorados,
+                tiempo_ms=tiempo_ms,
+                encontrada=True
+            )
+
+        if costo_actual > costo_minimo.get(nodo_actual, float("inf")):
+            continue
+
+        for arista in grafo.vecinos(nodo_actual):
+            nuevo_costo = costo_actual + arista.costo(alpha, beta)
+
+            if nuevo_costo < costo_minimo.get(arista.destino, float("inf")):
+                costo_minimo[arista.destino] = nuevo_costo
+                heapq.heappush(heap, (nuevo_costo, arista.destino, camino + [arista.destino]))
+
+        tiempo_ms = (time.perf_counter() - inicio) * 1000
+        #no encontramos la ruta
+        return ResultadoRuta(
+            algoritmo="BFS Adaptado",
+            ruta=[],
+            costo_total=float("inf"),
+            nodos_explorados=nodos_explorados,
+            tiempo_ms=tiempo_ms,
+            encontrada=False
+            )
